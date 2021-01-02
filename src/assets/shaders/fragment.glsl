c9d25360fc2e535f968bf60c8b5b68dcb345dcba
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2021-01-02 17:27:30 +0100

    Add first sphere and stick to scene

--- src/assets/shaders/fragment.glsl
+++ src/assets/shaders/fragment.glsl
@@ -10,21 +10,66 @@ uniform float uTime;
 uniform vec2 uResolution;
 varying vec2 vUv;
 
+vec2 map(vec3 point,float time);
+
+/*
+* Helpers
+*/
+vec3 calcNormal( vec3 point, float time )
+{
+    vec2 e = vec2(1.0,-1.0)*0.5773*0.001;
+    return normalize( e.xyy*map( point + e.xyy, time ).x +
+    e.yyx*map( point + e.yyx, time ).x +
+    e.yxy*map( point + e.yxy, time ).x +
+    e.xxx*map( point + e.xxx, time ).x );
+}
+
+float smin( float a, float b, float k )
+{
+    float h = clamp( 0.5+0.5*(b-a)/k, 0.0, 1.0 );
+    return mix( b, a, h ) - k*h*(1.0-h);
+}
+/*
+* Helpers, end
+*/
+
 /*
 * SDFs
 */
 float sdSphere(vec3 point,float radius){
     return length(point)-radius;
 }
-/*
-* SDFs, end
-*/
+
+vec2 sdStick(vec3 point, vec3 a, vec3 b, float r1, float r2) // approximated
+{
+    vec3 pa = point-a, ba = b-a;
+    float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+    return vec2( length( pa - ba*h ) - mix(r1,r2,h*h*(3.0-2.0*h)), h );
+}
 
 // Scene definition happens here
 vec2 map(vec3 point,float time){
-    float sdfId=1.;
-    return vec2(sdSphere(point,.01),sdfId);
+    float distScene = -1.;
+    float sdfId=-1.;
+
+    // first sphere
+    float sphereRadius = 0.05;
+    float distSphere = sdSphere(point,sphereRadius);
+    distScene = distSphere;
+    sdfId = 1.;
+
+    // first stick
+    vec3 stickStart = vec3(-1.,0.,0.);
+    vec3 stickEnd = vec3(0.,0.,-sphereRadius);
+    float distStick = sdStick(point,stickStart, stickEnd, 0.001,0.02).x;
+    sdfId = distStick < distScene ? 2. : sdfId;
+    distScene = smin(distScene, distStick, 0.3);
+
+    return vec2(distScene,sdfId);
 }
+/*
+* SDFs, end
+*/
 
 // Raymarching happens here
 // returns vector with (distance to scene, id of closest sdf)
@@ -63,8 +108,12 @@ void main(){
     vec3 colour=mix(vec3(.2),vec3(0.),length(vUv-vec2(.5)));
 
     // Check what sdfId the raymarchResult returns, e.g. raymarchResult.y>.5 means that we hit id 0
-    if(raymarchResult.y>.5){
-        colour=vec3(1.);
+    if(raymarchResult.y>1.5){
+        vec3 normal=calcNormal(rayDirection*raymarchResult.x+cameraPosition,uTime);
+        colour=vec3(1.,1.,0.);
+    }else if(raymarchResult.y>.5){
+        vec3 normal=calcNormal(rayDirection*raymarchResult.x+cameraPosition,uTime);
+        colour=vec3(dot(vec3(1.),normal));
     }
 
     // Results
